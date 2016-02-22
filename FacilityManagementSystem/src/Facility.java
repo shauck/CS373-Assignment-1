77e387ea23e7fdb1114f67b39b85b194a8b12a45
samhauck <shauck@luc.edu>
2016-02-21 23:11:12 -0600

    updated entire facility class to work with database

--- FacilityManagementSystem/src/Facility.java
+++ FacilityManagementSystem/src/Facility.java
@@ -19,8 +19,8 @@ public class Facility {
     public String usageSchedule;
     public float usageRate;
     public int problemCounter;
-    Connection c = DriverManager.getConnection("jdbc:sqlite:FacilityManagementSystem.db");
-    Statement stmt = null;
+    public Connection c = DriverManager.getConnection("jdbc:sqlite:FacilityManagementSystem.db");
+    public Statement stmt = null;
 
     public Facility(int ID, String facilityName, String facilityManager, String maintenenceSchedule, String usageSchedule, float usageRate, int problemCounter, Connection c){
         this.ID = ID;
@@ -51,35 +51,121 @@ public class Facility {
     }
 
 
-    public void changeName(String newName) {
-        facilityName = newName;
+    public void changeName(Facility facility, String newName) {
+        this.facilityName = newName;
+        try{
+            openConnection(c);
+            stmt = c.createStatement();
+            String sql = "UPDATE Facilities set NAME = ? where ID = ?";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1, this.facilityName);
+            ps.setInt(2, this.ID);
+
+        } catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
 
-    public void changeManager(String newManager) {
-        facilityManager = newManager;
+    public void changeManager(Facility facility, String newManager) {
+        this.facilityManager = newManager;
+        try{
+            openConnection(c);
+            stmt = c.createStatement();
+            String sql = "UPDATE Facilities set MANAGER = ? where ID = ?";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1, this.facilityManager);
+            ps.setInt(2, this.ID);
+
+        } catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
 
-    public void changeMaintenenceSchedule(String newSchedule) {
-        maintenenceSchedule = newSchedule;
+    public void changeMaintenenceSchedule(Facility facility, String newSchedule) {
+        this.maintenenceSchedule = newSchedule;
+        try{
+            openConnection(c);
+            stmt = c.createStatement();
+            String sql = "UPDATE Facilities set MAINTENANCESCHEDULE = ? where ID = ?";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1, this.maintenenceSchedule);
+            ps.setInt(2, this.ID);
+
+        } catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
 
 
-    public void changeUsageSchedule(String newSchedule) {
-        usageSchedule = newSchedule;
+    public void changeUsageSchedule(Facility facility, String newSchedule) {
+        this.usageSchedule = newSchedule;
+        try{
+            openConnection(c);
+            stmt = c.createStatement();
+            String sql = "UPDATE Facilities set USAGESCHEDULE = ? where ID = ?";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1, this.usageSchedule);
+            ps.setInt(2, this.ID);
+
+        } catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
 
-    public void changeUsageRate(Float newRate) {
-        usageRate = newRate;
+    public void changeUsageRate(Facility facility, Float newRate) {
+        this.usageRate = newRate;
+        try{
+            openConnection(c);
+            stmt = c.createStatement();
+            String sql = "UPDATE Facilities set USAGERATE = ? where ID = ?";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setFloat(1, this.usageRate);
+            ps.setInt(2, this.ID);
+
+        } catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
 
 
-    public String getFacilityInformation(Facility facility) {
-        String info = ("Facility Name: " + facility.facilityName + "\nFacility Manager: " +
-                facility.facilityManager + "\nMaintenence Schedule: " + facility.maintenenceSchedule +
-                "\nUsage Rate: " + facility.usageRate);
+    public String getFacilityInformation(Facility facility, int id) {
+        try{
+            openConnection(c);
+            stmt = c.createStatement();
+            String sql = "SELECT * FROM Facilities where ID = ?";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setInt(1, this.ID);
 
-
-        return info;
+            ResultSet rs = ps.executeQuery(sql);
+            while (rs.next()){
+                int i = rs.getInt("ID");
+                String name = rs.getString("NAME");
+                String manager = rs.getString("MANAGER");
+                String mSchedule = rs.getString("MAINTENANCESCHEDULE");
+                String uSchedule = rs.getString("USAGESCHEDULE");
+                Float rate = rs.getFloat("USAGERATE");
+                int problems = rs.getInt("PROBLEMCOUNT");
+                System.out.println("ID = " + i);
+                System.out.println("Facility name = " + name);
+                System.out.println("Facility Manager = " + manager);
+                System.out.println("Maintenance Schedule = " + mSchedule);
+                System.out.println("Usage Schedule = " + uSchedule);
+                System.out.println("Usage Rate = "+ rate);
+                System.out.println("No. of Facility Problems = " + problems);
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
 
     public void openConnection(Connection c) {
